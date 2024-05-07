b0218401e3467208f6b0a60a05c7533953c461d4
Aakash Jamadar <149180139+AakashJamadar@users.noreply.github.com>
2024-05-07 10:43:12 +0530

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    //This is a comment
+
+    /*this
+    is
+    a 
+    multi line
+    comment*/
+
+    std::cout<<"I like Pizza!"<<'\n';
+    std::cout<<"bring me some"<<'\n';
+    return 0;
+}
