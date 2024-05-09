e7753df2c95a2026f17817c799a3142239836a3c
Aakash Jamadar <149180139+AakashJamadar@users.noreply.github.com>
2024-05-09 14:28:01 +0530

    Add files via upload

---
+++ mathFunctions.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+    double x=3.9;
+    double y=4;
+    double z;
+    //z=std::max(x,y);
+    //z=std::min(x,y);
+    //z=pow(2,4);
+    //z=sqrt(25);
+    //z=abs(-6);  //returns it's distance from 0(i,e returns it's positive value)
+    //z=round(x);  //round offs to nearest integer
+    //z=ceil(x);
+    //z=floor(x);
+    std::cout<<z;
+    return 0;
+}
