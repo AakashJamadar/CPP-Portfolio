492e4cb0c9e8cc9acb04f7a62bff0d3f1d657893
Aakash Jamadar <149180139+AakashJamadar@users.noreply.github.com>
2024-05-11 11:20:34 +0530

    Add files via upload

---
+++ check.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    bool sunny=false;
+    if(!sunny){
+        std::cout<<"it is sunny outside";
+    }else{
+        std::cout<<"it is cloudy outside";
+    }
+    return 0;
+}
