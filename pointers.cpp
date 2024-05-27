ec09ec6cf977a0c490154850dda5a415b6c4cdc8
Aakash Jamadar <149180139+AakashJamadar@users.noreply.github.com>
2024-05-27 23:33:50 +0530

    Add files via upload

--- pointers.cpp
+++ pointers.cpp
@@ -23,13 +23,15 @@ int main(){
 
     std::string freePizzas [5]={"pizza1","pizza2","pizza3","pizza4","pizza5"};
     std::string* pFreePizzas=freePizzas;
-    std::cout<<(&freePizzas)<<'\n';  //prints the memory address of the entire array.
-    //array name already prints the address, so no need to add & again
-    std::cout<<pFreePizzas<<'\n';  //prints the memory address of the first element of the array.
-    //They are printing the same memory addresses,so let's use static_cast
+    // std::cout<<(&freePizzas)<<'\n';  //prints the memory address of the entire array.
+    // //array name already prints the address, so no need to add '&' again,
+    // std::cout<<pFreePizzas<<'\n';  //prints the memory address of the first element of the array. ( because, & is not used)
+    // //They are printing the same memory addresses,so let's use static_cast
     // std::cout<<static_cast<void*>(&freePizzas)<<'\n';
     // std::cout<<static_cast<void*>(pFreePizzas)<<'\n';  //it also printed the same memory address
 
+    std::cout<<&freePizzas<<'\n';   
+    std::cout<<&pFreePizzas<<'\n';  //prints the address in memory where the pointer variable pFreePizzas is stored.
     std::cout<<*(&freePizzas)<<'\n'; //outputs the memory addresss as it points to the memory address of the entire array and it can't print the entire array
     std::cout<<*pFreePizzas<<'\n';   //prints the first element of the array as it points to first element's memory address
     return 0;
