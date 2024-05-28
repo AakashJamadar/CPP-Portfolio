c022293254cc8c694f7d7186405083b0503e2250
Aakash Jamadar <149180139+AakashJamadar@users.noreply.github.com>
2024-05-28 20:20:47 +0530

    Add files via upload

--- hypotenuseOfATriangle.cpp
+++ hypotenuseOfATriangle.cpp
@@ -5,11 +5,11 @@ int main(){
     double a;
     double b;
     double c;
-    std::cout<<"Enter the length of side AB in the triangle ABC ";
+    std::cout<<"Enter the length of side AB in the triangle ABC: ";
     std::cin>>a;
-    std::cout<<"Enter the leghth of side BC in the trinagle ABC ";
+    std::cout<<"Enter the leghth of side BC in the trinagle ABC: ";
     std::cin>>b;
     c=sqrt(pow(a,2)+pow(b,2));
-    std::cout<<"Length of the hypotenuse of triangle ABC is "<<c;
+    std::cout<<"Length of the hypotenuse of triangle ABC is: "<<c;
     return 0;
 }
