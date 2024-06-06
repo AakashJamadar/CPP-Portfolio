7527d4f33cfbea38ed33ee7b3e56bc0ce751a840
Aakash Jamadar <149180139+AakashJamadar@users.noreply.github.com>
2024-06-06 23:21:39 +0530

    Add files via upload

--- structs.cpp
+++ structs.cpp
@@ -1,87 +1,53 @@
-// #include <iostream>
+// Input Format
 
-// struct student{            //student is like a datatype now
-//     std::string name;
-//     float gpa;
-//     bool enrolled=true;         //can set default to true,then we no need to use enrolled member in our student
-// };                         //remember to add ';' here
+// Input will consist of four lines.
+// The first line will contain an integer, representing age.
+// The second line will contain a string, consisting of lower-case Latin characters ('a'-'z'), representing the first_name of a student.
+// The third line will contain another string, consisting of lower-case Latin characters ('a'-'z'), representing the last_name of a student.
+// The fourth line will contain an integer, representing the standard of student.
 
-// int main(){
-//     //struct(structures)=A structure that groups related variables under one name
-//     //structs can contain many different data types(stru=ing,int,double,bool etc.)
-//     //variables in a struct are known as "members"
-//     //members can be accessed with . "Class member access operator."
+// Note: The number of characters in first_name and last_name will not exceed 50.
 
-//     student student1;
-//     student1.name="Sirsio";
-//     student1.gpa=3.45;
-    
-//     std::cout<<student1.name<<'\n';
-//     std::cout<<student1.gpa<<'\n';
-//     std::cout<<student1.enrolled<<'\n';
+// Output Format
 
-//     student student2;
-//     student2.name="Hinga";
-//     student2.gpa=2.3;
-//     student2.enrolled=true;
+// Output will be of a single line, consisting of age, first_name, last_name and standard, each separated by one white space.
 
-//     std::cout<<student2.name<<'\n';
-//     std::cout<<student2.gpa<<'\n';
-//     std::cout<<student2.enrolled<<'\n';  
+// P.S.: I/O will be handled by HackerRank.
 
-//     student student3;
-//     student3.name="Capri";
-//     student3.gpa=1.3;
-//     student3.enrolled=-5;  //Any non zero value evaluates to true
+// Sample Input
 
-//     std::cout<<student3.name<<'\n';
-//     std::cout<<student3.gpa<<'\n';
-//     std::cout<<student3.enrolled<<'\n'; 
-//     return 0;
-// }
+// 15
+// john
+// carmack
+// 10
+// Sample Output
 
-//passing struct as a argument(it is passed by value as default)
+// 15 john carmack 10
 
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
+using namespace std;
 
-struct car{
-     std::string model;
-    int year;
-    std::string colour;
+/*
+    add code for struct here.
+
+*/
+struct Student{
+    int age;
+    string first_name;
+    string last_name;
+    int standard;
 };
-//structs are passed by value not passed by reference,for proof output address of  a struct after passing it to function and before
 
-void printCar(car &carr){  //if i use & here which means pass by reference,the address in int main and here will be same.
-    std::cout<<&carr<<'\n';  //memory address will be different here as the output here will be the address of the copy
-    std::cout<<carr.model<<'\n';//bacause the original struct isn't passed directly
-    std::cout<<carr.year<<'\n';
-    std::cout<<carr.colour<<'\n';
-}
-
-void paintCar(car &carr,std::string colour){
-    carr.colour=colour;
-}
-//If struct is not passed by reference,we change the colour of the copy as we recieve copy only when struct is passed by value
-//So if we want to make changes in the original and want to see the changes we made,use '&'(adderess of operator) to pass by reference.
-
-int main(){
-    car car1;
-    car car2;
-
-    car1.model="Mustang";
-    car1.year=1964;
-    car1.colour="Black";
-
-    car2.model="Corvette";
-    car2.year=1953;
-    car2.colour="White";
-
-    paintCar(car1,"Silver");
-    paintCar(car2,"Gold");
-
-    //std::cout<<&car1<<'\n';
-    printCar(car1);
-    printCar(car2);
-
+int main() {
+    Student st;
+    
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+    
     return 0;
 }
