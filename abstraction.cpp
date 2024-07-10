e41b520dbb27cb836d6fb4d9589dbf015fc0c0f5
Aakash Jamadar <149180139+AakashJamadar@users.noreply.github.com>
2024-07-10 23:38:39 +0530

    Add files via upload

---
+++ abstraction.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+//Abstraction:It involves hiding the complex implementation details of a system and exposing only the necessary and relevant parts to the user. 
+
+//Advantages
+
+//1.only you can make changes to your data or functions and else no one can as other don't even have the idea of it
+//2.It makes the application secure by not allowing anyone else to see the background details
+//3.Increases the reusability of the code,ie avoids the duplication of the code
+
+//Implementation example is getter and setter for private members
