51a709024d18d7db7d8e911f38d2fc20be56a258
Aakash Jamadar <149180139+AakashJamadar@users.noreply.github.com>
2024-09-12 18:45:27 +0530

    Add files via upload

--- namespace.cpp
+++ namespace.cpp
@@ -1,29 +1,14 @@
-// #include <iostream> 
+#include <iostream>
 
-// namespace first{
-//     int x=1;
-// }
-
-// namespace second{
-//     int x=2;
-// }
-
-// int main(){
-//     using namespace second;
-    
-//     std::cout<<x;
-
-//     return 0;
-// }
-
-#include <iostream> 
+namespace first{
+    int x=5;
+}
+namespace second{
+    int x=7;
+}
 
 int main(){
-    using std::string;  //can be used for a particular name 
-    using namespace std; //inludes all names which require std 
-
-    string name="Harold";
-    cout<<"hello "<<name;
-
+    using namespace first;
+    std::cout<<x;  // second::x/first::x
     return 0;
 }
