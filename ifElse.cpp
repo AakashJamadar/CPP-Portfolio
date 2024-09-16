d339811435737dd292767c37c5f643f1dec88ad8
Aakash Jamadar <149180139+AakashJamadar@users.noreply.github.com>
2024-09-16 21:09:10 +0530

    Add files via upload

---
+++ ifElse.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+// int main()
+// {
+//     int s = 3;
+//     if(s == 3) 
+//     std::cout<<"Triangle\n";
+//     s = 4;
+//     else if(s == 4)
+//     std::cout<<"Rectangle\n";
+//     return 0;
+// }
+
+// int main()
+// {
+//     int s = 3;
+//     if(s == 3) 
+//     std::cout << "Triangle\n";
+//     int a = 4;
+//     std::cout << a; 
+//     return 0;
+// }
+
+// int main(){
+//     int i = 1;
+//     for( ; i <= 5; ){
+//         if(i <= 5){
+//            std::cout<<"Hi\n"; 
+//         }
+//         i++;
+//     }
+//     return 0;
+// }
+
+// int main(){
+//     int n;
+//     if(std::cin >> n){
+//         std::cout << "Hello\n";
+//     }
+//     return 0;
+// }
+
+int main(){
+    int n = 4;
+    if(std::cout << n){
+        std::cout << "Hello\n";
+    }
+    return 0;
+}
